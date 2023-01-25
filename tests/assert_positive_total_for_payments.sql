a20d377e260fe8bad87a4e864dca48a6b7258697
Akli CHELLALA <akli.chellala@talan.com>
2023-01-25 15:57:34 +0100

    my first project bdt snowflake

---
+++ tests/assert_positive_total_for_payments.sql
@@ -0,0 +1,9 @@
+with payements as(
+    select * from {{ ref('stg_payments') }}
+)
+select
+    order_id,
+    sum(amount) as total_amount
+from {{ ref('stg_payments') }}
+group by 1
+having not(total_amount >= 0)
